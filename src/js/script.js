6eaaf17f02cfb2b0afe6054031ce7c2df72c4fe6
Nih <nihomaris@gmail.com>
2019-02-01 00:19:33 +0400

    Main Page

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,34 @@
+/*--------------------------------------------------------------
+>>> CONTENTS:
+----------------------------------------------------------------
+	MAIN SCRIPTS
+
+	MAIN PAGE
+
+
+--------------------------------------------------------------*/
+
+/* ==========================================================================
+	MAIN SCRIPTS
+========================================================================== */
+
 jQuery(document).ready(function($) {
-    console.log('test');
+
+	/*  */
+    $('#top-section').on('mousemove', function(e) {
+		var lineWidth = parseInt($('#header-line').css('width'))/2,
+			maxWindowWidth = $(window).width()-lineWidth,
+			coordinateX = 0;
+
+		if(e.pageX < lineWidth) {
+			coordinateX = 0;
+		} else if(e.pageX > maxWindowWidth) {
+			coordinateX = maxWindowWidth-lineWidth;
+		} else {
+			coordinateX = e.pageX-lineWidth;
+		}
+
+    	$('#header-line').css({'left': coordinateX+'px'})
+
+    })
 });
