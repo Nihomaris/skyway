c3a323f2598079e76dc32a35b27f9274a208cb21
Nih <nihomaris@gmail.com>
2019-02-12 16:33:33 +0400

    Created Country and Tour page mobile\tablet

--- src/js/script.js
+++ src/js/script.js
@@ -5,6 +5,8 @@
 
 	MAIN PAGE
 
+	TOUR PAGE
+
 
 --------------------------------------------------------------*/
 
@@ -14,7 +16,7 @@
 
 jQuery(document).ready(function($) {
 
-	/*  */
+	/* Декоративная бегующая полоска под хэдэром */
     $('#top-section').on('mousemove', function(e) {
 		var lineWidth = parseInt($('#header-line').css('width'))/2,
 			maxWindowWidth = $(window).width()-lineWidth,
@@ -31,4 +33,60 @@ jQuery(document).ready(function($) {
     	$('#header-line').css({'left': coordinateX+'px'})
 
     })
+	
+	/* Переключение меню */
+	$('#menu_toggle').on('click', function() {
+		$('#main_menu').css('display', 'block')
+	})
+	$('#close_main_menu').on('click', function() {
+		$('#main_menu').css('display', 'none')
+	})
+   	
+	/* Фиксированный хэдер */
+
+    
+});
+
+
+/* ==========================================================================
+	TOUR PAGE
+========================================================================== */
+
+
+
+
+jQuery(document).ready(function($) {
+	
+	/* Раскрывающийся список деталей тура */
+	$('.tour_shedule__name').on('click', function() {
+		var $container = $(this).parent(),
+			$detailBlock = $container.find('.tour_shedule__day_detail'),
+			$separator = $container.find('.tour_shedule__separator');
+
+		if($detailBlock.hasClass('tour_shedule__day_detail--open'))	{
+			$detailBlock.css('display', 'none').removeClass('tour_shedule__day_detail--open');
+			$separator.css('display', 'none');
+		} else {
+			$detailBlock.css('display', 'block').addClass('tour_shedule__day_detail--open');
+			$separator.css('display', 'block');
+		}
+		
+		
+	})
+	/* Раскрыть все детали */
+	$('#all_tour_btn').on('click', function() {
+		var $detailBlocks = $('.tour_shedule__day_detail'),
+			$separators = $('.tour_shedule__separator');
+			if($(this).hasClass('tour_shedule_all_btn--open')) {
+			$detailBlocks.css('display', 'none').removeClass('tour_shedule__day_detail--open');
+			$separators.css('display', 'none');
+			$(this).removeClass('tour_shedule_all_btn--open');
+		} else {
+			$detailBlocks.css('display', 'block').addClass('tour_shedule__day_detail--open');
+			$separators.css('display', 'block');
+			$(this).addClass('tour_shedule_all_btn--open');
+		}
+		
+	})
+
 });
